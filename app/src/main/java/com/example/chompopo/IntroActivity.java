1a0db75912509d645d6e982db91ba47a0c122c5b
Bang <piporica200@gmail.com>
2019-11-09 04:27:55 +0900

    intro alpha animation

--- app/src/main/java/com/example/chompopo/IntroActivity.java
+++ app/src/main/java/com/example/chompopo/IntroActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class IntroActivity extends AppCompatActivity {
     @Override
@@ -11,6 +14,14 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_layout);
 
+        //애니메이션 부분
+        final ImageView iv = (ImageView)findViewById(R.id.introText);
+        Animation animation = AnimationUtils.loadAnimation(
+                getApplicationContext(), R.anim.intro_anim);
+
+        iv.startAnimation(animation);
+
+        //인트로 5초 핸들러
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
