7d16be08c0f8e377b125fb7e9afb0fc93fb0c2d0
Bang <piporica200@gmail.com>
2019-11-09 04:11:45 +0900

    bgchange

--- app/src/main/java/com/example/chompopo/IntroActivity.java
+++ app/src/main/java/com/example/chompopo/IntroActivity.java
@@ -1,4 +1,33 @@
 package com.example.chompopo;
 
-public class IntroActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_layout);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        finish();
+    }
+
 }
