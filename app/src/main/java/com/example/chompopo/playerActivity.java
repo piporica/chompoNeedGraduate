600d22625be977324ec9067ea2acf7dbb58820a7
Bang <piporica200@gmail.com>
2019-11-10 03:28:24 +0900

    player layout1

--- app/src/main/java/com/example/chompopo/playerActivity.java
+++ app/src/main/java/com/example/chompopo/playerActivity.java
@@ -1,4 +1,30 @@
 package com.example.chompopo;
 
+import android.os.Handler;
+import android.widget.ImageView;
+
 public class playerActivity {
+
+    //마커 깜빡임 구현 함수
+    public void blinkCircle(final ImageView iv){
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            int i = 1;
+            int timer;
+            @Override
+            public void run() {
+                if(i<0)
+                {
+
+                }
+
+            }
+        });
+
+    }
+
+    //음표움직임 구현 함수
+
+    //if 속도(어떻게 할 건지?)가 얼마 이상이면 duration 바꿀 것
+
 }
